a3217ae937669c97ce170bf01e991f07327e4b5e
unknown <jonray007@yahoo.com>
2017-06-29 13:00:33 -0500

    first

---
+++ src/js/pages/Featured.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class Featured extends React.Component {
+  render() {
+    return (
+    <div> Featured</div>
+    );
+  }
+}
---
+++ src/js/pages/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "react-router"
+
+
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>Killer News and bees</h1>
+      <h1>Killer News and bees</h1>
+      {<Link to="archives">archives</Link>}
+      {/*<Link></Link>*/}
+      {2+2}
+    </div>
+    );
+  }
+}
